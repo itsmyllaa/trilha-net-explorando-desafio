762625ba11f130d3ba7448bc91acd5e1398a245e
Camila Xavier <camila.gabriela.xavier.santos@gmail.com>
2024-05-29 21:58:00 -0300

    Subindo projeto completo Hospedagem de Hotel

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -8,7 +8,6 @@ namespace DesafioProjetoHospedagem.Models
             Capacidade = capacidade;
             ValorDiaria = valorDiaria;
         }
-
         public string TipoSuite { get; set; }
         public int Capacidade { get; set; }
         public decimal ValorDiaria { get; set; }
